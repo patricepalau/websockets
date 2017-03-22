f2cdd51881abbe0c620d714e94f6c980068244c2
Patrice Palau <patrice.palau@oracle.com>
2017-03-22 10:27:06 -0400

    sample code with multiple clients

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ A simple websocket example, using:
 * websocket (https://github.com/theturtle32/WebSocket-Node) on the server
 * and ootb web sockets on the client
 
+Run the sample server code, and open multiple browser windows pointing at client.html.
+The server will broadcast incoming client messages to all other connected clients.
+
 
--- server/app.js
+++ server/app.js
@@ -18,18 +18,30 @@ server.listen(port, function () {
   console.log(`${now} - server listening on ${port}`);
 });
 
+var connections = [];
+
 webSocketServer.on('request', function (request) {
   var connection = request.accept('foo', request.origin);
+  connections.push(connection);
   console.log('Connection accepted');
   
   connection.on('message', function (message) {
     const now = new Date();
     console.log(`${now} - received message:`, message);
-    connection.sendUTF('Hello from server!');
+    const data = JSON.parse(message.utf8Data);
+    // redispatching to all other clients
+    connections.forEach(function (conn) {
+      if (conn !== connection) {
+        conn.sendUTF(`Hello from server - redispatching message from client: ${data.id}!`);
+      }
+    })
   });
   
   connection.on('close', function (code, description) {
     const now = new Date();
     console.log(`${now} - connection closed by remote host ${connection.remoteAddress}`);
+    connections.filter(function (conn) {
+      retur (conn !== connection);
+    })
   });
 });
